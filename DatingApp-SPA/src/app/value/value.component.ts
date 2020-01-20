18b188d29512ef684fef96d3e723903363bb245b
Nnaji Victor <nnajivictor0@gmail.com>
2020-01-20 17:59:16 +0100

    include angukar file

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  constructor(private https: HttpClient) { }
+
+  value: any;
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.https.get('http://localhost:5000/api/values').subscribe(response => {
+      this.value = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
