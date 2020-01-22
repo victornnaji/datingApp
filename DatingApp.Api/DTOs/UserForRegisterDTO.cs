d83f4fa658b1c0d51c17de0e2206d972ceb6a31b
Nnaji Victor <nnajivictor0@gmail.com>
2020-01-22 22:41:07 +0100

    added validation for user

--- DatingApp.Api/DTOs/UserForRegisterDTO.cs
+++ DatingApp.Api/DTOs/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.Api.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage="Minimum of 4 and maximum of 8")]
         public string password { get; set; }
     }
 }
